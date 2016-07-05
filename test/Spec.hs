97da7d720460112e878979cb856b9cc82a85ceba
Sean Riddle <swr@fastmail.fm>
2016-07-04 20:16:31 -0700

    Test framework should work now (but fail, intentionally)

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+import Test.HUnit
+import Test.Framework
+import Test.Framework.Providers.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain [testCase "Successful test" simpleAssertion
+                  , testCase "Failed test" wrongAssertion]
+simpleAssertion :: Assertion
+simpleAssertion = 5 @=? 5
+
+wrongAssertion :: Assertion
+wrongAssertion = 5 @=? 4
+
