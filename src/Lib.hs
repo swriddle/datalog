e0315ebe0e3467e31ec98e405d4951c43dfa5f1f
Sean Riddle <swr@fastmail.fm>
2016-07-06 19:09:22 -0700

    got all tests passing

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,13 +5,21 @@ module Lib
     , testConstantsContributeUniverse
     , testVariablesDoNotContributeUniverse
     , testTupleUniverse
+    , testUniverseBody
     ) where
+
+import qualified Data.List (nub)
 
 type VariableName = String
 type ConstantName = String
 type Universe = [ConstantName]
 type PredicateName = String
 type Tuple = (PredicateName, [Atom])
+type Literal = (Bool, Tuple)
+type Body = [Literal]
+type Head = Tuple
+data Rule = Fact Tuple
+          | ComplexRule Head Body
 
 nameOfConstant :: Atom -> ConstantName
 nameOfConstant (Constant constantName) = constantName
@@ -24,6 +32,11 @@ testConstantsContributeUniverse = universeAtom (Constant "abc")
 
 testVariablesDoNotContributeUniverse :: Universe
 testVariablesDoNotContributeUniverse = universeAtom (Variable "X")
+
+testUniverseBody :: Universe
+testUniverseBody = universeBody [(True, ("f", [Constant "abc", Variable "X"])),
+                                 (True, ("g", [Constant "def", Variable "Y"])),
+                                 (False, ("f", [Variable "X", Variable "Y"]))]
 
 testTupleUniverse :: Universe
 testTupleUniverse = universeTuple ("pred", [(Constant "abc"), (Variable "X"), (Constant "xyz"), (Variable "Y")])
@@ -38,6 +51,16 @@ isConstant (Variable _) = False
 
 tupleConstants :: Tuple -> [Atom]
 tupleConstants (predName, atoms) = filter isConstant atoms
+
+universeLiteral :: Literal -> Universe
+universeLiteral (_, tuple) = universeTuple tuple
+
+universeBody :: Body -> Universe
+universeBody literals = foldr (++) [] (map universeLiteral literals)
+
+--universeRule :: Rule -> Universe
+--universeRule (Fact tuple) = universeTuple tuple
+--universeRule (ComplexRule tuple [literal]) = Data.List.nub $ (universeTuple tuple) ++ (universe
 
 universeTuple :: Tuple -> Universe
 universeTuple tuple = map nameOfConstant (tupleConstants tuple) 
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,15 +5,19 @@ import Test.Framework.Providers.HUnit
 import Lib
 
 constantsContribute :: Assertion
-constantsContribute = testConstantsContributeUniverse  @=? ["abc"]
+constantsContribute = testConstantsContributeUniverse  @?= ["abc"]
 
 variablesDoNotContribute :: Assertion
-variablesDoNotContribute = testVariablesDoNotContributeUniverse @=? []
+variablesDoNotContribute = testVariablesDoNotContributeUniverse @?= []
+
+bodyUniverseIsCorrect :: Assertion
+bodyUniverseIsCorrect = testUniverseBody @?= ["abc", "def"]
 
 tupleUniverseRight :: Assertion
-tupleUniverseRight = testTupleUniverse @=? ["abc", "xyz"]
+tupleUniverseRight = testTupleUniverse @?= ["abc", "xyz"]
 
 main :: IO ()
 main = defaultMain [testCase "universe contribution of 'abc' is ['abc']" constantsContribute
                   , testCase "universe contribution of 'X' is []" variablesDoNotContribute
-                  , testCase "tuple universe is right" tupleUniverseRight]
+                  , testCase "tuple universe is right" tupleUniverseRight
+                  , testCase "body universe is right" bodyUniverseIsCorrect]
